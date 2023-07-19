7cb9644cf463190f0960cf9149324c27f17b4fa9
Tushar Chaurasia <77012813+Dark-CodeX@users.noreply.github.com>
2023-07-19 13:23:05 +0530

    checks if cli app is present or not

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const { join } = require("path");
+const fs = require("fs");
+const os = require("os");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -46,14 +48,16 @@ const createWindow = () => {
     });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+    /* CHECK IF RUNCPP CLI EXISTS*/
+    if (fs.existsSync(os.platform() === "win32" ? process.cwd() + "/runcpp.exe" : process.cwd() + "/runcpp") === false) {
+        dialog.showErrorBox("RUNCPP CLI Not Found", "'" + (os.platform() === "win32" ? process.cwd() + "/runcpp.exe" : process.cwd() + "/runcpp") + "' was not found, try visiting https://github.com/Dark-CodeX/runcpp");
+        process.exit(1);
+    }
+    /* CHECK IF RUNCPP CLI EXISTS*/
+    createWindow();
+});
 
-// Quit when all windows are closed, except on macOS. There, it"s common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
         app.quit();
@@ -61,12 +65,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-    // On OS X it"s common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
